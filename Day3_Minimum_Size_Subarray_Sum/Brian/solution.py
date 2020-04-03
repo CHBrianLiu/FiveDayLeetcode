f5df261d723014d386383baf179579868bd4bc08
Brian Liu <CHBrian.Liu@outlook.com>
2020-04-03 20:33:45 +0800

    [Brian] Add Day3 solution.

--- Day3_Minimum_Size_Subarray_Sum/Brian/solution.py
+++ Day3_Minimum_Size_Subarray_Sum/Brian/solution.py
@@ -5,3 +5,39 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        if len(nums) == 0:
+            return 0
+        if s == 0:
+            return 1
+
+        if sum(nums) < s:
+            return 0
+
+        start, end = 0, 1
+        min_subarray = len(nums)
+
+        while start < len(nums) and end <= len(nums):
+            total = sum(nums[start:end])
+            # The sum of subarray has reached target.
+            # Narrow the subarray by moving 'start'.
+            if total >= s:
+                if end - start < min_subarray:
+                    min_subarray = end - start
+                if start < end:
+                    start += 1
+                else:
+                    start += 1
+                    end += 1
+            # The sum of subarray hasn't reached target.
+            # Expand the subarray by moving 'end'.
+            else:
+                end += 1
+
+        return min_subarray
+
+
+if __name__ == "__main__":
+    runner = Solution()
+    s = 100
+    nums = [1, 1]
+    print(runner.minSubArrayLen(s, nums))
