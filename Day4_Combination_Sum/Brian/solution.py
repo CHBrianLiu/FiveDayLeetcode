7f0af3a95574220e1e9b10c44d808a3ec0fcee21
Brian Liu <CHBrian.Liu@outlook.com>
2020-04-04 21:11:07 +0800

    [Brian] Add day4 solution.

--- Day4_Combination_Sum/Brian/solution.py
+++ Day4_Combination_Sum/Brian/solution.py
@@ -1,3 +1,6 @@
+import copy
+
+
 class Solution(object):
     def combinationSum(self, candidates, target):
         """
@@ -5,3 +8,36 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
+        if len(candidates) == 0 or target == 0:
+            return []
+
+        candidates.sort()
+        self.ans = []
+
+        self.try_combination(target, candidates, [])
+
+        return self.ans
+
+    def try_combination(
+            self, remainds, availables, previous_combination):
+
+        for index in xrange(len(availables)):
+            current_combination = copy.deepcopy(previous_combination)
+            current_combination.append(availables[index])
+
+            if remainds > availables[index]:
+                self.try_combination(remainds - availables[index],
+                                     availables[index:],
+                                     current_combination)
+            elif remainds == availables[index]:
+                self.ans.append(current_combination)
+                return
+            else:
+                return
+
+
+if __name__ == "__main__":
+    runner = Solution()
+    candidates = [1, 2]
+    target = 1
+    print(runner.combinationSum(candidates, target))
